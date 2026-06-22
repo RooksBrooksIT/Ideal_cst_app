0d145345e906fc7f741be8ff6a53d7250f3d6960
rookssoft2025 <rookssoft2025@example.com>
2026-06-22 14:30:23 +0530

    roro commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'screens/splash_screen.dart';
+import 'screens/lets_start_page.dart';
+import 'screens/main_dashboard.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Construct Pro',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color.fromARGB(255, 2, 32, 116),
+        hintColor: Colors.grey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          iconTheme: IconThemeData(color: Colors.blueGrey),
+        ),
+      ),
+      // Define initial route
+      initialRoute: '/',
+      // Define app routes
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/letsStart': (context) => const LetsStartPage(),
+        '/dashboard': (context) => MainDashboard(),
+      },
+    );
+  }
+}
